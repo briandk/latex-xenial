fe493755354bcb631c505ed13a539bd3aae7a2c9
Brian Danielak <briandaniela.k+github@gmail.com>
2016-03-17 15:18:51 -0400

    execute apt-get install

--- install-texlive-without-docs.py
+++ install-texlive-without-docs.py
@@ -1,5 +1,4 @@
 import subprocess
-import re
 
 get_texlive_dependencies = subprocess.run(
     ["apt-cache",
@@ -10,22 +9,23 @@ get_texlive_dependencies = subprocess.run(
 )
 
 dependency_pattern = "Depends: "
-non_doc_dependencies = []
-
-for dependency in get_texlive_dependencies.stdout.splitlines():
-    dependency = dependency.strip()
-    if dependency.startswith(dependency_pattern) and not dependency.endswith("-doc"):
-        non_doc_dependencies.append(
-            dependency.strip().replace(dependency_pattern, ""))
 
 def extract_dependency(dependency_text, pattern):
     dependency = dependency_text.strip().replace(pattern, "")
     return(dependency)
 
-x = [extract_dependency(line, dependency_pattern)
+dependencies = [extract_dependency(line, dependency_pattern)
         for line in get_texlive_dependencies.stdout.splitlines()
         if line.strip().startswith(dependency_pattern) and not line.strip().endswith("-doc")]
 
-print(len(non_doc_dependencies))
-print(len(x))
-print(x == non_doc_dependencies)
+
+arguments = [
+    "apt-get",
+    "install",
+    "--assume-yes"
+]
+
+arguments.extend(dependencies)
+
+# execute apt-get install with all the package names
+subprocess.run(arguments)
